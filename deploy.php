a05bb2e325a2f41df650957b3318ebad6dcc50c0
Jeremy Bell <jeremytm@gmail.com>
2015-11-10 00:12:33 +1300

    New deploy stuff

--- deploy.php
+++ deploy.php
@@ -1,12 +1,4 @@
 <?php
 
-$last_line = system('ember deploy -prod', $output);
-
-if (strpos($last_line, 'Uploaded revision:') !== false) {
-  
-    $revision = trim(explode(": ", $last_line)[1]);
-    echo "Activating revision: " . $revision . "\n";
-    system('ember deploy:activate --revision ' . $revision . ' -prod');
-    
-}
+system('ember deploy -prod', $output);
 
