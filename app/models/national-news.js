745d2a63bbd656e560a41ad0acac3eb3f25f56f6
Jeremy Bell <jeremytm@gmail.com>
2015-11-16 21:01:43 +1300

    Add decorated manager names to dashboard news

--- app/models/national-news.js
+++ app/models/national-news.js
@@ -6,7 +6,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
   country: DS.belongsTo('country',{ async: true }),
-  author: DS.belongsTo('manager',{ async: true }),
+  author: DS.belongsTo('manager',{ async: false }), // Get straight away so we can decorate users
   lastViewed: DS.attr('date'),
   isNewToUser: Ember.computed('lastViewed', function() {
     return this.get('lastViewed') && this.get('lastViewed') < this.get('date');
