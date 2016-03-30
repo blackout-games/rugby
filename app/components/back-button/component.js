bac33b66490bd7367742bcd1fed694024ae675e9
Jeremy Bell <jeremytm@gmail.com>
2016-03-30 17:19:21 +1300

    Added back button to player page (goes back to squad page)

--- app/components/back-button/component.js
+++ app/components/back-button/component.js
@@ -7,10 +7,20 @@ export default Ember.Component.extend({
   
   classNames: 'back-button-component',
   
+  toId: null,
+  
   /**
    * Set this on the component to provide a default back route when the first landing page is a page with a back button
    */
   default: "dashboard",
+  
+  setup: Ember.on('didInsertElement',function(){
+    
+    if(this.get('colored')){
+      this.$('a').removeClass('on-dark');
+    }
+    
+  }),
   
   actions: {
     goBack() {
@@ -18,10 +28,14 @@ export default Ember.Component.extend({
       this.get('history').goBack(this.get('default'));
       
     },
-    goBackTo(to) {
+    goBackTo(to,toId) {
       
       var app = getOwner(this).lookup('route:application');
-      app.transitionTo(to);
+      if(toId){
+        app.transitionTo(to,toId);
+      } else {
+        app.transitionTo(to);
+      }
       
     },
   }
