fa0fb7207a46c1d66ffc691544e0d67d0ae9885d
Jeremy Bell <jeremytm@gmail.com>
2016-04-13 16:51:50 +1200

    Bug fixes for datetime-picker. Add support for minDate and maxDate

--- app/components/datetime-picker/component.js
+++ app/components/datetime-picker/component.js
@@ -4,18 +4,24 @@ export default Ember.Component.extend({
   
   initDate: Ember.on('didReceiveAttrs',function(opts){
     if(opts.newAttrs.date){
+      
       let date = this.get('date');
+      
       if(!date){
         date = new Date();
       } else {
         date = this.assertDate(date);
       }
+      
       this.set('timeDate',date);
       this.set('dateDate',date);
+      
     } else if(!this.get('timeDate')){
+      
       let date = new Date();
       this.set('timeDate',date);
       this.set('dateDate',date);
+      
     }
   }),
   
@@ -23,13 +29,23 @@ export default Ember.Component.extend({
     get() {
       let date = this.get('dateDate');
       let time = this.get('timeDate');
+      
+      if(!date.setHours){
+        Ember.Logger.warn('An invalid date has been passed to datetime-picker');
+        return new Date();
+      }
+      
       date.setHours(time.getHours());
       date.setMinutes(time.getMinutes());
+      
       return new Date(date.getTime());
     },
     set(key, value) {
+      value = this.assertDate(value);
+      
       this.set('timeDate',  value);
       this.set('dateDate',  value);
+      
       return value;
     }
   }),
@@ -53,7 +69,14 @@ export default Ember.Component.extend({
   
   assertDate(date){
     if(!(date instanceof Date)){
-      date = new Date(date);
+      if(date){
+        date = new Date(date);
+      } else {
+        date = new Date();
+      }
+    } else {
+      // We must copy any date objects to ensure dates passed in are readonly
+      date = new Date(date.getTime());
     }
     return date;
   },
