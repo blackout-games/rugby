682f2ecbac7b8eae8a720ad6406a102a207f7cea
Jeremy Bell <jeremytm@gmail.com>
2016-04-16 01:19:58 +1200

    If we land on the player sale page and the player isn't for sale, send to player index

--- app/players/player/sale/route.js
+++ app/players/player/sale/route.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   noScrollReset:true,
+  
+  afterModel(model/*, transition*/){
+    if(model.player){
+      model = model.player;
+    }
+    if(!model.get('transfer')){
+      this.transitionTo('players.player',model.get('id'));
+    }
+  },
 });
