aa53e144657e9e0e02fcceea524e76667f23a4b8
Jeremy Bell <jeremytm@gmail.com>
2016-04-18 13:22:37 +1200

    Add owned-club helper
    Pass in a club, and it will let you know if the club is owned by the
    current user.

---
+++ app/helpers/owned-club.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export function ownedClub(params/*, hash*/) {
+  
+  let session = this.get('session');
+  let store = this.get('store');
+  let club = params[0];
+  
+  if(session.get('isAuthenticated')){
+    let manager = store.peekRecord('manager',this.get('session.data.manager.id'));
+    let clubFound = manager.get('clubs').findBy('id',club.get('id'));
+    return clubFound;
+  }
+  
+  return false;
+}
+
+export default Ember.Helper.extend({
+  session: Ember.inject.service(),
+  store: Ember.inject.service(),
+  compute: ownedClub
+});
---
+++ tests/unit/helpers/owned-club-test.js
@@ -0,0 +1,10 @@
+import { ownedClub } from 'rugby-ember/helpers/owned-club';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | owned club');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = ownedClub([42]);
+  assert.ok(result);
+});
