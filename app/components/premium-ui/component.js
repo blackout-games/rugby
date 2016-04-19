d105802225c3d24320b8d25ef84e8b50dd434f00
Jeremy Bell <jeremytm@gmail.com>
2016-04-19 23:34:46 +1200

    Add premium UI component

---
+++ app/components/premium-ui/component.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  
+  classNameBindings: ['classNamesIfHidden'],
+  
+  classNamesIfHidden: Ember.computed('classIfHidden','hideFeature',function(){
+    if(this.get('hideFeature') && this.get('classIfHidden')){
+      return this.get('classIfHidden');
+    } else {
+      return false;
+    }
+  }),
+  
+  hideFeature: Ember.computed('active',function(){
+    return this.get('active') && !this.get('session.isPremium');
+  }),
+  
+});
