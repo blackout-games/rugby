297b5e8ec4dad61d45ba5a44c64de63b6357e090
Jeremy Bell <jeremytm@gmail.com>
2016-04-19 23:34:17 +1200

    Initialize club-link with own club if no other club specified

--- app/components/club-link/component.js
+++ app/components/club-link/component.js
@@ -5,6 +5,19 @@ export default Ember.Component.extend({
   attributeBindings: ['href'],
   classNames: ['btn-a'],
   imageSize: 'medium',
+  hasInit: false,
+  
+  setup: Ember.on('didReceiveAttrs',function(){
+    if(!this.get('hasInit')){
+      if(!this.get('club') && this.get('session.isAuthenticated')){
+        
+        // Use current user club
+        this.set('club',this.get('session.club'));
+        
+      }
+      this.set('hasInit',true);
+    }
+  }),
   
   href: Ember.computed('club',function(){
     return 'https://www.blackoutrugby.com/game/club.lobby.php?id=' + this.get('club.id');
--- app/components/club-ui/component.js
+++ app/components/club-ui/component.js
@@ -4,4 +4,18 @@ export default Ember.Component.extend({
   classNames: ['inline-block'],
   imageSize: 'medium',
   tagName: 'span',
+  hasInit: false,
+  
+  setup: Ember.on('didReceiveAttrs',function(){
+    if(!this.get('hasInit')){
+      if(!this.get('club') && this.get('session.isAuthenticated')){
+        
+        // Use current user club
+        this.set('club',this.get('session.club'));
+        
+      }
+      this.set('hasInit',true);
+    }
+  }),
+  
 });
