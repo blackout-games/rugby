afb3ea56351872f5ed4c6a47913ed456df57d80e
Jeremy Bell <jeremytm@gmail.com>
2016-04-21 16:53:45 +1200

    Message UI shows by default, and supports yield.

--- app/components/message-ui/component.js
+++ app/components/message-ui/component.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
   classNames: ['gap-lg','color-bg-text'],
   classNameBindings: ['type','hideIf:hidden'],
   
+  showIf: true,
   hideIf: Ember.computed.not('showIf'),
   
 });
