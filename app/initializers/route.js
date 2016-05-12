c71e9f5cda814369880026438a743c3f147290ae
Jeremy Bell <jeremytm@gmail.com>
2016-05-12 17:41:12 +1200

    Add forced loader bar for player route.

--- app/initializers/route.js
+++ app/initializers/route.js
@@ -3,18 +3,38 @@ import Ember from 'ember';
 let forceLoaderRoutes = [
   //{ name: 'squad.club', authenticated: true },
   { name: 'squad.club' },
-  //{ name: 'players.player' },
+  { name: 'players.player' },
 ];
 
-let isForcedLoaderRoute = function( routeName, authenticated ){
+let isForcedLoaderRoute = function( routeName, authenticated, router, newParams ){
   
   let found = false;
   
+  // Remove index
+  let fullRoute = routeName;
+  if(routeName.substr(-6)==='.index'){
+    routeName = routeName.substr(0,routeName.length-6);
+  }
+  
   Ember.$.each(forceLoaderRoutes,(index,route)=>{
     
-    if(route.name === routeName && (!route.authenticated || authenticated ) ){
-      found = true;
-      return false;
+    if(routeName.indexOf(route.name) === 0 && (!route.authenticated || authenticated )){
+      
+      let currentRoute = router.get('currentPath');
+      let params = router.get('router.state.params');
+      let subRoute = route.name+'.';
+      let oldParams = [];
+      
+      if(params[route.name]){
+        oldParams = Object.keys(params[route.name]).map(key => params[route.name][key]);
+        
+      }
+      
+      // Make sure not coming from subroute, or if we are, the params need to have changed
+      if(currentRoute.indexOf(subRoute)<0 || (fullRoute===currentRoute && oldParams!==newParams)){
+        found = true;
+        return false;
+      }
     }
     
   });
@@ -32,15 +52,17 @@ var blackoutRouter = {
   _doTransition(){
     
     let name = arguments[0];
+    let params = arguments[1];
     
     /**
      * This allows us to force the loader to appear on certain routes, even when not returning a promise, or thier promise is already fulfilled.
      * Example, the squad page, since it takes so long to render it's helpful for the user to see a loading bar.
      */
     
-    if(!this.get('_forceNormalTransitionTo') && isForcedLoaderRoute(name,this.get('session.isAuthenticated')) ){
+    
+    if(!this.get('_forceNormalTransitionTo') && isForcedLoaderRoute(name,this.get('session.isAuthenticated'), this, params) ){
       
-      let currentRoute = Ember.Blackout.getCurrentRoute();
+      let currentRoute = this.get('currentPath');
       
       // Remove any indexes
       currentRoute = currentRoute.replace(/\.index/g,'');
