423d976e19ff688432f03b803596cd377d318c7c
Jeremy Bell <jeremytm@gmail.com>
2016-06-20 21:57:50 +1200

    Minor tweaks to club rankings layout

---
+++ tests/unit/helpers/safe-test.js
@@ -0,0 +1,10 @@
+import { safe } from 'rugby-ember/helpers/safe';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | safe');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = safe([42]);
+  assert.ok(result);
+});
